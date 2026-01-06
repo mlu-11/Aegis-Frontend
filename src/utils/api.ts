d5075d69f9a9ce724b7e678d2049c3e0808d8d15
mlu-11 <mengyaolu11@gmail.com>
2026-01-06 13:18:13 +0100

    update env variables

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,10 @@
 //
-const API_BASE_URL = "http://localhost:5000/api";
+// const API_BASE_URL =
+//   (import.meta.env.VITE_API_URL as string) || "http://localhost:5000/api";
 
+const RAW_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+const API_BASE_URL = RAW_BASE.replace(/\/$/, "") + "/api";
 class API {
   token: string | null = localStorage.getItem("token");
 
